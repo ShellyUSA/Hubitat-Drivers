6cf781e0087f8cc455f2cf2776ecf47e079b7175
Daniel Winks <daniel.winks@gmail.com>
2024-02-21 12:56:43 -0500

    Update ShellyButton1Blu.groovy

--- Bluetooth/ShellyButton1Blu.groovy
+++ Bluetooth/ShellyButton1Blu.groovy
@@ -11,7 +11,9 @@ metadata {
     capability 'Battery' //battery - NUMBER, unit:%
     capability 'PushableButton' //numberOfButtons - NUMBER //pushed - NUMBER
     capability 'HoldableButton' //held - NUMBER
+    capability 'PresenceSensor' //presence - ENUM ["present", "not present"]
     attribute 'lastUpdated', 'string'
+    command 'checkPresence'
   }
 }
 @Field static Boolean BLU = true
@@ -19,5 +21,15 @@ void initialize() { configure() }
 void configure() {
   this.device.setDeviceNetworkId(getDeviceSettings().macAddress.replace(':','').toUpperCase())
   getDevice().updateSetting('macAddress', [type: 'string', value: getDeviceSettings().macAddress.replace(':','').toUpperCase()])
+  if(getDeviceSettings().presenceTimeout == null) {getDevice().updateSetting('presenceTimeout', [type: 'number', value: 300])}
   this.device.sendEvent(name: 'numberOfButtons', value: 3)
+  runEvery1Minute('checkPresence')
+}
+
+@CompileStatic
+void checkPresence() {
+  Event lastEvent = getDevice().events([max: 1])[0]
+  if(((unixTimeMillis() - lastEvent.getUnixTime()) / 1000) > (getDeviceSettings().presenceTimeout as Integer)) {
+    getDevice().sendEvent([name: 'presence', value: 'not present', isStateChange: false])
+  }
 }
