5831535943d505f17d487252c2fa617ac75b99d7
Daniel Winks <daniel.winks@gmail.com>
2024-04-03 18:49:48 -0400

    Update ShellyMotion2.groovy

--- WebhookWebsocket/ShellyMotion2.groovy
+++ WebhookWebsocket/ShellyMotion2.groovy
@@ -3,19 +3,22 @@
 metadata {
   definition (name: 'Shelly Motion 2', namespace: 'ShellyUSA', author: 'Daniel Winks', importUrl: '') {
     capability 'Initialize'
+<<<<<<< Updated upstream
+=======
+    capability 'Configuration'
+>>>>>>> Stashed changes
     capability 'Battery' //battery - NUMBER, unit:%
     capability 'MotionSensor' //motion - ENUM ['inactive', 'active']
     capability 'TamperAlert' //tamper - ENUM ['clear', 'detected']
     capability 'IlluminanceMeasurement' //illuminance - NUMBER, unit:lx
     capability 'TemperatureMeasurement' //temperature - NUMBER, unit:°F || °C
     attribute 'lastUpdated', 'string'
-
     command 'getPrefsFromDevice'
   }
 }
 
-if(device != null) {preferences{}}
 @Field static Boolean GEN1 = true
+<<<<<<< Updated upstream
 // =============================================================================
 // Initialize And Configure
 // =============================================================================
@@ -117,4 +120,19 @@ void setDeviceActionsGen1() {
     }
   }
 }
+=======
+@Field static Boolean HAS_BATTERY_GEN1 = true
+@Field static Boolean HAS_LUX_GEN1 = true
+@Field static Boolean HAS_TEMP_GEN1 = true
+@Field static Boolean HAS_MOTION_GEN1 = true
+@Field static List<String> ACTIONS_TO_CREATE = [
+  'motion_off',
+  'motion_on',
+  'tamper_alarm_off',
+  'tamper_alarm_on',
+  'bright_condition',
+  'dark_condition',
+  'twilight_condition'
+]
+>>>>>>> Stashed changes
 
