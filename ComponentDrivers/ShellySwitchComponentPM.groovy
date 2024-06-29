20b774d16c94f271362af393b24e47e561e088dc
Daniel Winks <daniel.winks@gmail.com>
2024-06-29 19:43:17 -0400

    Update ShellySwitchComponentPM.groovy

--- ComponentDrivers/ShellySwitchComponentPM.groovy
+++ ComponentDrivers/ShellySwitchComponentPM.groovy
@@ -9,18 +9,17 @@ metadata {
     capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
     capability 'EnergyMeter' //energy - NUMBER, unit:kWh
     command 'resetEnergyMonitors'
+    command 'getPrefsFromDevice'
   }
 }
 
 if(device != null) {preferences{}}
 @Field static Boolean COMP = true
-
 // =============================================================================
 // Device Specific
 // =============================================================================
 void on() {parent?.postCommandSync(switchSetCommand(true, getDeviceDataValue('switchId') as Integer))}
 void off() {parent?.postCommandSync(switchSetCommand(false, getDeviceDataValue('switchId') as Integer))}
-void refresh() {parent?.postCommandSync(switchGetStatusCommand(getDeviceDataValue('switchId') as Integer))}
 // =============================================================================
 // End Device Specific
 // =============================================================================
