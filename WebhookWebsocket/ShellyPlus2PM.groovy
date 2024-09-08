1a9ba752d71ba6761bf9f41f66bf8799402192d5
Daniel Winks <daniel.winks@gmail.com>
2024-09-08 13:52:42 -0400

    Update ShellyPlus2PM.groovy

--- WebhookWebsocket/ShellyPlus2PM.groovy
+++ WebhookWebsocket/ShellyPlus2PM.groovy
@@ -4,6 +4,7 @@ metadata {
   definition (name: 'Shelly Plus 2 PM (websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Switch'
     capability 'Initialize'
+
     capability 'Configuration'
     capability 'Refresh'
     capability 'CurrentMeter' //amperage - NUMBER, unit:A
@@ -26,8 +27,6 @@ void on() { postCommandSync(switchSetCommand(true)) }
 
 @CompileStatic
 void off() { postCommandSync(switchSetCommand(false)) }
-
-
 
 void refreshDeviceSpecificInfo() {
   switchGetConfig()
