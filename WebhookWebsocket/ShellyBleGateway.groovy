4e4f604c52c03080921889b3b3aefad71f50c498
Daniel Winks <daniel.winks@gmail.com>
2024-12-25 11:51:10 -0500

    Update ShellyBleGateway.groovy

--- WebhookWebsocket/ShellyBleGateway.groovy
+++ WebhookWebsocket/ShellyBleGateway.groovy
@@ -1,7 +1,7 @@
 #include ShellyUSA.ShellyUSA_Driver_Library
 
 metadata {
-  definition (name: 'Shelly BLE Gateway', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+  definition (name: 'Shelly BLE Gateway (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Initialize'
     capability 'Configuration'
     attribute 'lastUpdated', 'string'
@@ -9,40 +9,3 @@ metadata {
 }
 
 if(device != null) {preferences{}}
-
-// =============================================================================
-// Initialization
-// =============================================================================
-void initialize() {
-  if(hasIpAddress()) {
-    atomicState.initInProgress = true
-    initializeWebsocketConnection()
-  }
-  if(getDeviceSettings().enableBluetooteGateway == null) { this.device.updateSetting('enableBluetooteGateway', true) }
-  configure()
-}
-
-void configure() {
-  if(getDeviceSettings().enableBluetooteGateway == true) {enableBluReportingToHE()}
-  else if(getDeviceSettings().enableBluetooteGateway == false) {disableBluReportingToHE()}
-}
-// =============================================================================
-// End Initialization
-// =============================================================================
-
-
-
-// =============================================================================
-// Device Specific
-// =============================================================================
-void parse(String message) {
-  LinkedHashMap json = (LinkedHashMap)slurper.parseText(message)
-  processWebsocketMessagesAuth(json)
-  processWebsocketMessagesConnectivity(json)
-  processWebsocketMessagesBluetoothEvents(json)
-  logJson(json)
-  setLastUpdated()
-}
-// =============================================================================
-// End Device Specific
-// =============================================================================
