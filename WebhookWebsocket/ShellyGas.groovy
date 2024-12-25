9d5937f1a5ee2941d3b6788d1a4b8b00cd66e06a
Daniel Winks <daniel.winks@gmail.com>
2024-12-25 11:51:17 -0500

    Update ShellyGas.groovy

--- WebhookWebsocket/ShellyGas.groovy
+++ WebhookWebsocket/ShellyGas.groovy
@@ -16,25 +16,11 @@ metadata {
   }
 }
 
-if(device != null) {preferences{}}
 @Field static Boolean GEN1 = true
-// =============================================================================
-// Initialize And Configure
-// =============================================================================
-@CompileStatic
-void initialize() {configure()}
+if(device != null) {preferences{}}
 
 @CompileStatic
-void configure() {allDevicesConfiguration()}
-
-@CompileStatic
-void parse(String raw) {parseGen1Message(raw)}
-
-@CompileStatic
-void refresh() {getStatus()}
-
-@CompileStatic
-void getStatus() {
+void refreshDeviceSpecificInfo() {
   LinkedHashMap response = (LinkedHashMap)sendGen1Command('status')
   logJson(response)
   LinkedHashMap concentration = (LinkedHashMap)response?.concentration
@@ -43,18 +29,6 @@ void getStatus() {
   LinkedHashMap gas_sensor = (LinkedHashMap)response?.gas_sensor
   String self_test_state = gas_sensor?.self_test_state.toString()
   getDevice().sendEvent(name: 'selfTestState', value: self_test_state)
-}
-
-@CompileStatic
-void setDeviceActionsGen1() {
-  LinkedHashMap actions = getDeviceActionsGen1()
-  logJson(actions)
-  actions.each{k,v ->
-    String queryString = 'index=0&enabled=true'
-    queryString += "&name=${k}".toString()
-    queryString += "&urls[]=${getHubBaseUri()}/${((String)k).replace('_url','')}".toString()
-    sendGen1Command('settings/actions', queryString)
-  }
 }
 
 @CompileStatic
