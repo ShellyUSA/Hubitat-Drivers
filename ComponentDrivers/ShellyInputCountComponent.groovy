7582eab7a69f47951abf84b2a3f773ad8e681287
Daniel Winks <daniel.winks@gmail.com>
2024-12-25 06:32:06 -0500

    Create ShellyInputCountComponent.groovy

---
+++ ComponentDrivers/ShellyInputCountComponent.groovy
@@ -0,0 +1,24 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Input Count Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Sensor'
+    capability 'Refresh'
+
+    attribute 'count', 'number'
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+if(device != null) {preferences{}}
+@Field static Boolean COMP = true
+// =============================================================================
+// Device Specific
+// =============================================================================
+void on() {logWarn('Cannot change state of an input on a Shelly device from Hubitat!')}
+void off() {logWarn('Cannot change state of an input on a Shelly device from Hubitat!')}
+// =============================================================================
+// End Device Specific
+// =============================================================================
