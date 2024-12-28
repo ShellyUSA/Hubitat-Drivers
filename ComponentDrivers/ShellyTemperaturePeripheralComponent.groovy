9d570b96b91aace7859ce629433bd922f194d145
Daniel Winks <daniel.winks@gmail.com>
2024-12-28 12:51:24 -0500

    Create ShellyTemperaturePeripheralComponent.groovy

---
+++ ComponentDrivers/ShellyTemperaturePeripheralComponent.groovy
@@ -0,0 +1,12 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Temperature Peripheral Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability "TemperatureMeasurement" //temperature - NUMBER, unit:°F || °C
+
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+if(device != null) {preferences{}}
+@Field static Boolean COMP = true
