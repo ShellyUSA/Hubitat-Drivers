96c4be9a0cf231c24ccffb1043b79228c49b3afd
Daniel Winks <daniel.winks@gmail.com>
2025-01-01 19:50:34 -0500

    Create ShellyUni.groovy

---
+++ WebhookWebsocket/ShellyUni.groovy
@@ -0,0 +1,34 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Uni (Webhook)', namespace: 'ShellyUSA', author: 'Daniel Winks', importUrl: '') {
+    capability 'Configuration'
+    command 'setDeviceActionsGen1'
+    command 'getPreferencesFromShellyDeviceGen1'
+    command 'deleteHubitatWebhooksGen1'
+    command 'deleteChildDevices'
+
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+@Field static Boolean GEN1 = true
+@Field static Boolean HAS_ADC_GEN1 = true
+@Field static Boolean HAS_EXT_TEMP_GEN1 = true
+@Field static Boolean HAS_EXT_HUM_GEN1 = true
+@Field static List<String> ACTIONS_TO_CREATE = [
+  'out_on_url',
+  'out_off_url',
+  'btn_on_url',
+  'btn_off_url',
+  'longpush_url',
+  'shortpush_url',
+  'adc_over_url',
+  'adc_under_url',
+  'report_url',
+  'ext_temp_over_url',
+  'ext_temp_under_url',
+  'ext_hum_over_url',
+  'ext_hum_under_url'
+]
+if(device != null) {preferences{}}
