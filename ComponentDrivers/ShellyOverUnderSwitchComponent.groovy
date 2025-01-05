5ad7b496db4fabc965128c853ad6bba17b4eeb26
Daniel Winks <daniel.winks@gmail.com>
2025-01-05 17:24:13 -0500

    Create ShellyOverUnderSwitchComponent.groovy

---
+++ ComponentDrivers/ShellyOverUnderSwitchComponent.groovy
@@ -0,0 +1,27 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly OverUnder Switch Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Switch'
+    capability 'Refresh'
+
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+if(device != null) {preferences{}}
+@Field static Boolean COMP = true
+// =============================================================================
+// Device Specific
+// =============================================================================
+void on() {
+  logWarn('Cannot change state of an OverUnder on a Shelly device from Hubitat!')
+  sendEvent(name: 'switch', value: 'off', isStateChange: false)
+  }
+void off() {
+  logWarn('Cannot change state of an OverUnder on a Shelly device from Hubitat!')
+  sendEvent(name: 'switch', value: 'on', isStateChange: false)
+  }
+// =============================================================================
+// End Device Specific
+// =============================================================================
