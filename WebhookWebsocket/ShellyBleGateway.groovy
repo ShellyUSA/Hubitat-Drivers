9e3bf0d1e4a8cb79de2803fde940c25402e8b786
Daniel Winks <daniel.winks@gmail.com>
2025-01-19 17:14:40 -0500

    Update ShellyBleGateway.groovy

--- WebhookWebsocket/ShellyBleGateway.groovy
+++ WebhookWebsocket/ShellyBleGateway.groovy
@@ -1,11 +1,12 @@
 #include ShellyUSA.ShellyUSA_Driver_Library
 
 metadata {
-  definition (name: 'Shelly BLE Gateway (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+  definition (name: 'Shelly Blu Gateway (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Initialize'
     capability 'Configuration'
     attribute 'lastUpdated', 'string'
   }
 }
 
-if(device != null) {preferences{}}
+@Field static Boolean WS = true
+@Field static Boolean NOCHILDREN = true
