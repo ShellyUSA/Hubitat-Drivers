7738ed9eb9b91e10b55821b2668fb126c4b8080d
Daniel Winks <daniel.winks@gmail.com>
2025-01-19 20:35:12 -0500

    Update ShellyInputButtonComponent.groovy

--- ComponentDrivers/ShellyInputButtonComponent.groovy
+++ ComponentDrivers/ShellyInputButtonComponent.groovy
@@ -2,20 +2,30 @@
 
 metadata {
   definition (name: 'Shelly Input Button Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
-    capability 'PushableButton'
+    capability 'PushableButton' //numberOfButtons - NUMBER, pushed - NUMBER
+    capability 'DoubleTapableButton' //doubleTapped - NUMBER
+    capability 'HoldableButton' //held - NUMBER
     capability 'Refresh'
 
     attribute 'lastUpdated', 'string'
   }
 }
 
-if(device != null) {preferences{}}
 @Field static Boolean COMP = true
 // =============================================================================
 // Device Specific
 // =============================================================================
-void on() {parentPostCommandSync(switchSetCommand(true, getDeviceDataValue('inputSwitchId') as Integer))}
-void off() {parentPostCommandSync(switchSetCommand(false, getDeviceDataValue('inputSwitchId') as Integer))}
+@CompileStatic
+void push(Integer buttonNumber) {sendDeviceEvent([name: 'pushed', value: buttonNumber, isStateChange: true]) }
+
+@CompileStatic
+void hold(Integer buttonNumber) {sendDeviceEvent([name: 'held', value: buttonNumber, isStateChange: true]) }
+
+@CompileStatic
+void doubleTap(Integer buttonNumber) {sendDeviceEvent([name: 'doubleTapped', value: buttonNumber, isStateChange: true]) }
+
+@CompileStatic
+void deviceSpecificConfigure() {sendDeviceEvent([name: 'numberOfButtons', value: 1])}
 // =============================================================================
 // End Device Specific
 // =============================================================================
