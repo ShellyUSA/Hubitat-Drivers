21ead37679c76cb1a939885a5ed500e9d9cb334f
Daniel Winks <daniel.winks@gmail.com>
2025-01-21 14:22:52 -0500

    Update ShellyPlus1.groovy

--- WebhookWebsocket/ShellyPlus1.groovy
+++ WebhookWebsocket/ShellyPlus1.groovy
@@ -1,8 +1,7 @@
 #include ShellyUSA.ShellyUSA_Driver_Library
 
 metadata {
-  definition (name: 'Shelly Plus 1 (Mini)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
-    capability 'Switch'
+  definition (name: 'Shelly Plus 1 & Plus 1 Mini', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Initialize'
     capability 'Configuration'
     capability 'Refresh'
