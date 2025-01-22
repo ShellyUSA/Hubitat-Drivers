615f054f50566725ec7fbc609dfba834cdf85769
Daniel Winks <daniel.winks@gmail.com>
2025-01-21 19:55:02 -0500

    Create ShellyBluGateway.groovy

---
+++ WebhookWebsocket/ShellyBluGateway.groovy
@@ -0,0 +1,12 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Blu Gateway (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean NOCHILDREN = true
