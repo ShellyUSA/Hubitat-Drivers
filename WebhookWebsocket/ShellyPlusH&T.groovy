5d5b8662056535909c368adada5bb9eb3da429d8
Daniel Winks <daniel.winks@gmail.com>
2025-01-24 12:17:03 -0500

    Update ShellyPlusH&T.groovy

--- WebhookWebsocket/ShellyPlusH&T.groovy
+++ WebhookWebsocket/ShellyPlusH&T.groovy
@@ -3,9 +3,9 @@
 metadata {
   definition (name: 'Shelly Plus H&T Gen 2&3 (Webhook)', namespace: 'ShellyUSA', author: 'Daniel Winks', importUrl: '') {
     capability 'Initialize'
-    capability "Battery" //battery - NUMBER, unit:%
-    capability "RelativeHumidityMeasurement" //humidity - NUMBER, unit:%rh
-    capability "TemperatureMeasurement" //temperature - NUMBER, unit:°F || °C
+    capability 'Battery' //battery - NUMBER, unit:%
+    capability 'RelativeHumidityMeasurement' //humidity - NUMBER, unit:%rh
+    capability 'TemperatureMeasurement' //temperature - NUMBER, unit:°F || °C
     attribute 'lastUpdated', 'string'
   }
 }
