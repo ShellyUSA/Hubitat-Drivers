6241ca9e52343167c912f7058d92b7b896c3d632
Daniel Winks <daniel.winks@gmail.com>
2025-01-24 17:50:25 -0500

    Create Shelly2.5.groovy

---
+++ WebhookWebsocket/Shelly2.5.groovy
@@ -0,0 +1,22 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly 2.5 (Webhook)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+  }
+}
+
+@Field static Boolean GEN1 = true
+@Field static List<String> ACTIONS_TO_CREATE = [
+  'out_on_url',
+  'out_off_url',
+  'btn_on_url',
+  'btn_off_url',
+  'longpush_url',
+  'shortpush_url',
+  'roller_open_url',
+  'roller_close_url',
+  'roller_stop_url'
+]
