66a91b409886f8d080622f93a5a85c84ec2a14e4
Daniel Winks <daniel.winks@gmail.com>
2025-01-24 20:49:30 -0500

    Create ShellyCoverComponentPM.groovy

---
+++ ComponentDrivers/ShellyCoverComponentPM.groovy
@@ -0,0 +1,18 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Cover PM Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'WindowShade' //windowShade - ENUM ['opening', 'partially open', 'closed', 'open', 'closing', 'unknown'] //position - NUMBER, unit:%
+    capability 'Refresh'
+    capability 'CurrentMeter' //amperage - NUMBER, unit:A
+    capability 'PowerMeter' //power - NUMBER, unit:W
+    capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+
+    attribute 'lastUpdated', 'string'
+  }
+}
+
+@Field static Boolean COMP = true
