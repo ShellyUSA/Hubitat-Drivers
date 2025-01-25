57c71c93ebfd7df4736f59c37510bb248b247d6f
Daniel Winks <daniel.winks@gmail.com>
2025-01-25 08:02:46 -0500

    Update ShellyPlus2PM.groovy

--- WebhookWebsocket/ShellyPlus2PM.groovy
+++ WebhookWebsocket/ShellyPlus2PM.groovy
@@ -4,7 +4,6 @@ metadata {
   definition (name: 'Shelly Plus 2 PM (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Switch'
     capability 'Initialize'
-
     capability 'Configuration'
     capability 'Refresh'
     capability 'CurrentMeter' //amperage - NUMBER, unit:A
