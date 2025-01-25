49b518dfc0a85df43ae1a9a34029d71881280890
Daniel Winks <daniel.winks@gmail.com>
2025-01-25 15:59:09 -0500

    Create ShellyProDualCoverPM.groovy

---
+++ WebhookWebsocket/ShellyProDualCoverPM.groovy
@@ -0,0 +1,13 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Pro Dual Cover PM (Websocket)', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
