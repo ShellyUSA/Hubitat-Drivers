8f40f926253e306d3508f2a32427372cbd93cada
Daniel Winks <daniel.winks@gmail.com>
2025-01-24 20:49:27 -0500

    Update ShellyCoverComponent.groovy

--- ComponentDrivers/ShellyCoverComponent.groovy
+++ ComponentDrivers/ShellyCoverComponent.groovy
@@ -4,7 +4,7 @@ metadata {
   definition (name: 'Shelly Cover Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Initialize'
     capability 'Configuration'
-    capability 'WindowShade' //windowShade - ENUM ["opening", "partially open", "closed", "open", "closing", "unknown"] //position - NUMBER, unit:%
+    capability 'WindowShade' //windowShade - ENUM ['opening', 'partially open', 'closed', 'open', 'closing', 'unknown'] //position - NUMBER, unit:%
     capability 'Refresh'
 
     attribute 'lastUpdated', 'string'
