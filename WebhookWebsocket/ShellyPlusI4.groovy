36a91816ae5ffaa2be8b9256dfa9c384bf606970
Daniel Winks <daniel.winks@gmail.com>
2025-01-28 20:35:36 -0500

    Create ShellyPlusI4.groovy

---
+++ WebhookWebsocket/ShellyPlusI4.groovy
@@ -0,0 +1,11 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Plus I4', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+  }
+}
+
+@Field static Boolean WS = true
