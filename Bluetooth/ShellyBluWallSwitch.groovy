bc533d8efa6fc74c5060620de37d72ecb2583891
Daniel Winks <daniel.winks@gmail.com>
2025-04-12 14:34:14 -0400

    Create ShellyBluWallSwitch.groovy

---
+++ Bluetooth/ShellyBluWallSwitch.groovy
@@ -0,0 +1,32 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition(
+    name: 'Shelly BLU Wall Switch 4',
+    namespace: 'ShellyUSA',
+    author: 'Daniel Winks',
+    component: true,
+    importUrl:''
+  ) {
+    capability 'Battery' //battery - NUMBER, unit:%
+    capability 'PushableButton' //numberOfButtons - NUMBER //pushed - NUMBER
+    capability 'DoubleTapableButton' //doubleTapped - NUMBER
+    capability 'HoldableButton' //held - NUMBER
+    capability 'ReleasableButton' //released - NUMBER
+    capability 'PresenceSensor' //presence - ENUM ["present", "not present"]
+
+    command 'tripleTap'
+    attribute 'tripleTapped', 'number'
+    attribute 'lastUpdated', 'string'
+  }
+}
+@Field static Boolean BLU = true
+@Field static Integer BUTTONS = 4
+
+@CompileStatic
+void deviceSpecificConfigure() {
+  if(getDeviceSettings().presenceTimeout == null) {setDeviceSetting('presenceTimeout', [type: 'number', value: 300])}
+  sendDeviceEvent([name: 'numberOfButtons', value: 4])
+  runEveryCustomSeconds(60, 'checkPresence')
+}
+
