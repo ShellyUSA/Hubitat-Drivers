0062ae487054a54c7b7c0c26e433a3aae86949ae
Daniel Winks <daniel.winks@gmail.com>
2025-04-12 14:29:56 -0400

    Create ShellyButton4Blu.groovy

---
+++ Bluetooth/ShellyButton4Blu.groovy
@@ -0,0 +1,31 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition(
+    name: 'Shelly Button 4 (Blu)',
+    namespace: 'ShellyUSA',
+    author: 'Daniel Winks',
+    component: true,
+    importUrl:''
+  ) {
+    capability 'Battery' //battery - NUMBER, unit:%
+    capability 'PushableButton' //numberOfButtons - NUMBER //pushed - NUMBER
+    capability 'DoubleTapableButton' //doubleTapped - NUMBER
+    capability 'HoldableButton' //held - NUMBER
+    capability 'ReleasableButton' //released - NUMBER
+    capability 'PresenceSensor' //presence - ENUM ["present", "not present"]
+
+    command 'tripleTap'
+    attribute 'tripleTapped', 'number'
+    attribute 'lastUpdated', 'string'
+  }
+}
+@Field static Boolean BLU = true
+@Field static Integer BUTTONS = 4
+
+@CompileStatic
+void deviceSpecificConfigure() {
+  if(getDeviceSettings().presenceTimeout == null) {setDeviceSetting('presenceTimeout', [type: 'number', value: 300])}
+  runEveryCustomSeconds(60, 'checkPresence')
+}
+
