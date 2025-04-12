02920b19c5b70416d559d830fb9f775f85c87a69
Daniel Winks <daniel.winks@gmail.com>
2025-04-12 14:30:04 -0400

    Create ShellyH&TBlu.groovy

---
+++ Bluetooth/ShellyH&TBlu.groovy
@@ -0,0 +1,18 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition(
+    name: 'Shelly H&T (Blu)',
+    namespace: 'ShellyUSA',
+    author: 'Daniel Winks',
+    component: true,
+    importUrl:''
+  ) {
+    capability 'Battery' //battery - NUMBER, unit:%
+    capability 'RelativeHumidityMeasurement' //humidity - NUMBER, unit:%rh
+    capability 'TemperatureMeasurement' //temperature - NUMBER, unit:°F || °C
+    attribute 'lastUpdated', 'string'
+  }
+}
+@Field static Boolean BLU = true
+void deviceSpecificConfigure() { }
