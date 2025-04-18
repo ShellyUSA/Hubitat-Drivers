cf1b79b28ce11a6124da92b2170a8eb895d03d69
Daniel Winks <daniel.winks@gmail.com>
2025-04-17 20:37:40 -0400

    Create ShellyPlus0-10vDimmer.groovy

---
+++ WebhookWebsocket/ShellyPlus0-10vDimmer.groovy
@@ -0,0 +1,13 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Plus 0-10v Dimmer', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
