41cfbd3473a02f1e32d6550085eed60fe3fe8d1f
Daniel Winks <daniel.winks@gmail.com>
2025-04-26 14:26:10 -0400

    Create ShellyPlusWallDimmer.groovy

---
+++ WebhookWebsocket/ShellyPlusWallDimmer.groovy
@@ -0,0 +1,16 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Plus Wall Dimmer', namespace: 'ShellyUSA', author: 'Daniel Winks', importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'Switch'
+    capability 'SwitchLevel' //level - NUMBER, unit:%
+    capability 'Light'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean NOCHILDREN = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
