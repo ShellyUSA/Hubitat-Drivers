b72cc69ff821cb6262e1077f77f28edbc78a3cf2
Daniel Winks <daniel.winks@gmail.com>
2025-04-27 06:03:29 -0400

    Create ShellyPro1PM.groovy

---
+++ WebhookWebsocket/ShellyPro1PM.groovy
@@ -0,0 +1,18 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Pro 1PM', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Switch'
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'CurrentMeter' //amperage - NUMBER, unit:A
+    capability 'PowerMeter' //power - NUMBER, unit:W
+    capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
