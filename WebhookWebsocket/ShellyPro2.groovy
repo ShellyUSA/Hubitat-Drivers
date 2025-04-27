31234fea392320ad0b76659b4041fed1684136f8
Daniel Winks <daniel.winks@gmail.com>
2025-04-27 06:04:53 -0400

    Create ShellyPro2.groovy

---
+++ WebhookWebsocket/ShellyPro2.groovy
@@ -0,0 +1,13 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Pro 2', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Switch'
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
