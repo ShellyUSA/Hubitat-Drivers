375a59510fbf5fb1c227ab93018aa19109c7f6dd
Daniel Winks <daniel.winks@gmail.com>
2025-04-27 06:05:30 -0400

    Create ShellyInputComponent.groovy

---
+++ ComponentDrivers/ShellyInputComponent.groovy
@@ -0,0 +1,20 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Input Button Component', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'PushableButton' //numberOfButtons - NUMBER, pushed - NUMBER
+    capability 'DoubleTapableButton' //doubleTapped - NUMBER
+    capability 'HoldableButton' //held - NUMBER
+    capability 'Refresh'
+    command 'tripleTap'
+    attribute 'tripleTapped', 'number'
+    capability 'Sensor'
+
+    attribute 'count', 'number'
+
+  }
+}
+
+@Field static Boolean COMP = true
+@Field static Integer BUTTONS = 1
+
