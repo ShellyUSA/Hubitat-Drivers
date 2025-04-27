42aa956db79011d9268b255f78bc560eab7b606b
Daniel Winks <daniel.winks@gmail.com>
2025-04-27 06:04:59 -0400

    Update ShellyPro4PM.groovy

--- WebhookWebsocket/ShellyPro4PM.groovy
+++ WebhookWebsocket/ShellyPro4PM.groovy
@@ -1,7 +1,7 @@
 #include ShellyUSA.ShellyUSA_Driver_Library
 
 metadata {
-  definition (name: 'Shelly Pro 4 PM', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+  definition (name: 'Shelly Pro 4PM', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
     capability 'Switch'
     capability 'Initialize'
     capability 'Configuration'
