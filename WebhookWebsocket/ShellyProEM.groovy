e165d31d0127046cbe490400d6b1b5b43076a485
Daniel Winks <daniel.winks@gmail.com>
2025-04-27 21:08:07 -0400

    Create ShellyProEM.groovy

---
+++ WebhookWebsocket/ShellyProEM.groovy
@@ -0,0 +1,17 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Pro EM', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    capability 'Switch'
+    attribute 'returnedEnergy', 'number' //unit:kWh
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
+@Field static Boolean EM1DATA = true
