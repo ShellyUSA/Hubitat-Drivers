3a8b6c6fa56688461f03bd88b3c9d56fcf54ad95
Daniel Winks <daniel.winks@gmail.com>
2025-04-29 07:31:14 -0400

    Create ShellyPro3EM400.groovy

---
+++ WebhookWebsocket/ShellyPro3EM400.groovy
@@ -0,0 +1,20 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly 3EM400', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'CurrentMeter' //amperage - NUMBER, unit:A
+    capability 'PowerMeter' //power - NUMBER, unit:W
+    capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    capability 'TemperatureMeasurement' //temperature - NUMBER, unit:°F || °C
+    attribute 'returnedEnergy', 'number' //unit:kWh
+    attribute 'apparentPower', 'number'
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
