cc7a22cd121affa24652cda3d142425a4038e1ca
Daniel Winks <daniel.winks@gmail.com>
2025-04-29 20:04:04 -0400

    Create ShellyEM.groovy

---
+++ WebhookWebsocket/ShellyEM.groovy
@@ -0,0 +1,21 @@
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly EM', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'CurrentMeter' //amperage - NUMBER, unit:A
+    capability 'PowerMeter' //power - NUMBER, unit:W
+    capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    capability 'Switch'
+    attribute 'returnedEnergy', 'number' //unit:kWh
+    attribute 'apparentPower', 'number'
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
+@Field static Boolean NOCHILDREN = true
