5c0b68929de7eb8e17a7c1edbf0a8b538ab17130
Daniel Winks <daniel.winks@gmail.com>
2026-02-09 18:40:50 -0500

    Add Shelly Plug US Gen4 driver file
    Add a new Groovy driver file for the Shelly Plug (US Gen4) under WebhookWebsocket. The file (version 2.16.0) includes the ShellyUSA driver library, defines device metadata with capabilities (Switch, Initialize, Configuration, Refresh, CurrentMeter, PowerMeter, VoltageMeasurement, EnergyMeter, IlluminanceMeasurement) and a resetEnergyMonitors command, and sets static fields WS, NOCHILDREN, and DEVICEISBLUGATEWAY.

---
+++ WebhookWebsocket/ShellyPlugUSGen4.groovy
@@ -0,0 +1,23 @@
+/**
+ * Version: 2.16.0
+ */
+#include ShellyUSA.ShellyUSA_Driver_Library
+
+metadata {
+  definition (name: 'Shelly Plug', namespace: 'ShellyUSA', author: 'Daniel Winks', singleThreaded: false, importUrl: '') {
+    capability 'Switch'
+    capability 'Initialize'
+    capability 'Configuration'
+    capability 'Refresh'
+    capability 'CurrentMeter' //amperage - NUMBER, unit:A
+    capability 'PowerMeter' //power - NUMBER, unit:W
+    capability 'VoltageMeasurement' //voltage - NUMBER, unit:V //frequency - NUMBER, unit:Hz
+    capability 'EnergyMeter' //energy - NUMBER, unit:kWh
+    capability 'IlluminanceMeasurement' //illuminance - NUMBER, unit:lux
+    command 'resetEnergyMonitors'
+  }
+}
+
+@Field static Boolean WS = true
+@Field static Boolean NOCHILDREN = true
+@Field static Boolean DEVICEISBLUGATEWAY = true
